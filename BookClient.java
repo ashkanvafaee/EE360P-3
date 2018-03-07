e488876ea6c562b880a456e53fde5c76742caa0d
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2018-03-06 18:09:20 -0600

    Add files via upload

---
+++ BookClient.java
@@ -0,0 +1,288 @@
+/*
+ * //UT-EID1= av28837
+ * //UT-EID2= rg36763
+ * 
+ */
+import java.util.Scanner;
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.*;
+public class BookClient {
+  public static void main (String[] args) {
+    String hostAddress;
+    int tcpPort;
+    int udpPort;
+    int clientId;
+    
+    String mode = "U";
+    Scanner din;
+    PrintStream pout;
+    Socket server;
+    
+    int len = 1024;
+    byte [] buf = new byte[len];
+    byte [] rbuf = new byte[len];
+
+    
+    
+    if (args.length != 2) {
+      System.out.println("ERROR: Provide 2 arguments: commandFile, clientId");
+      System.out.println("\t(1) <command-file>: file with commands to the server");
+      System.out.println("\t(2) client id: an integer between 1..9");
+      System.exit(-1);
+    }
+
+    String commandFile = args[0];
+    clientId = Integer.parseInt(args[1]);
+    hostAddress = "localhost";
+    tcpPort = 7000;// hardcoded -- must match the server's tcp port
+    udpPort = 8000;// hardcoded -- must match the server's udp port
+
+    
+   
+    
+    try {
+    	 PrintWriter out = new PrintWriter(new FileOutputStream("out_" + clientId + ".txt")); 
+    	
+        Scanner sc = new Scanner(new FileReader(commandFile));
+
+        while(sc.hasNextLine()) {
+          String cmd = sc.nextLine();
+          String[] tokens = cmd.split(" ");
+
+          if (tokens[0].equals("setmode")) {
+            // TODO: set the mode of communication for sending commands to the server 
+        	  mode = tokens[1];
+          }
+          else if (tokens[0].equals("borrow")) {
+            // TODO: send appropriate command to the server and display the
+            // appropriate responses form the server
+				String book = tokens[2];
+				
+				for(int i=3; i < tokens.length; i++) {
+					book += " " + tokens[i];
+				}
+        	  
+        	  if(mode.equals("T")) {
+        		  server = new Socket(hostAddress, tcpPort);
+        		  din = new Scanner(server.getInputStream());
+        		  pout = new PrintStream (server.getOutputStream());
+        		  
+        		  pout.println(tokens[0] + " " + tokens[1] + " " + book);
+        		  
+        		  out.println(din.nextLine());  
+        		  server.close();
+        	  }
+        	  else {
+        		  
+        		  DatagramSocket datasocket = new DatagramSocket();
+        		  buf = new byte[(tokens[0] + " " + tokens[1] + " " + book).length()];
+        		  buf = (tokens[0] + " " + tokens[1] + " " + book).getBytes();
+        		  DatagramPacket datapacket,returnpacket;
+        		  
+        		  datapacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(hostAddress), udpPort);
+        		  
+        		  datasocket.send(datapacket);
+        		  
+        		  
+        		  
+        		  returnpacket = new DatagramPacket(rbuf,rbuf.length);
+        		  datasocket.receive(returnpacket);
+        		  
+        		  String received = new String(returnpacket.getData());
+        		  received = received.substring(0, returnpacket.getLength());
+        		  
+        		  out.println(received);
+        		  
+        		  datasocket.close();
+        		  
+        		  
+        	  }
+        	  
+        	  
+          } else if (tokens[0].equals("return")) {
+            // TODO: send appropriate command to the server and display the
+            // appropriate responses form the server
+        	  
+        	  if(mode.equals("T")) {
+        		  server = new Socket(hostAddress, tcpPort);
+        		  pout = new PrintStream (server.getOutputStream());   	
+        		  din = new Scanner(server.getInputStream());
+        		  pout.println(tokens[0] + " " + tokens[1]);
+        		  
+        		  out.println(din.nextLine());
+        		  server.close();
+        	  }
+        	  else {
+        		  
+        		  DatagramSocket datasocket = new DatagramSocket();
+        		  buf = new byte[(tokens[0] + " " + tokens[1]).length()];
+        		  buf = (tokens[0] + " " + tokens[1]).getBytes();
+        		  DatagramPacket datapacket,returnpacket;
+        		          		  
+        		  datapacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(hostAddress), udpPort);
+        		  
+        		  datasocket.send(datapacket);
+        		  
+        		  returnpacket = new DatagramPacket(rbuf,rbuf.length);
+        		  datasocket.receive(returnpacket);
+        		  
+        		  String received = new String(returnpacket.getData());
+        		  received = received.substring(0, returnpacket.getLength());
+        		  
+        		  out.println(received);
+        		  
+        		  datasocket.close();
+        		  
+        	  }
+        	  
+          } else if (tokens[0].equals("inventory")) {
+            // TODO: send appropriate command to the server and display the
+        	  
+        	  if(mode.equals("T")) {
+        		  server = new Socket(hostAddress, tcpPort);
+        		  pout = new PrintStream (server.getOutputStream());   	
+        		  din = new Scanner(server.getInputStream());
+        		  pout.println(tokens[0]);
+        		  
+        		  String s = din.nextLine();
+        		  
+        		  while(!s.equals("done")) {
+        			  out.println(s);
+        			  s = din.nextLine();
+        		  }
+        		  
+        		  server.close();
+        		  
+        	  }
+        	  else {
+        		  
+        		  DatagramSocket datasocket = new DatagramSocket();
+        		  buf = new byte[(tokens[0]).length()];
+        		  buf = (tokens[0]).getBytes();
+        		  DatagramPacket datapacket,returnpacket;
+        		  
+        		  datapacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(hostAddress), udpPort);        		  
+        		  
+        		  datasocket.send(datapacket);
+        		  
+        		  returnpacket = new DatagramPacket(rbuf,rbuf.length);
+        		  datasocket.receive(returnpacket);
+        		  
+        		  String s = new String(returnpacket.getData());
+        		  s = s.substring(0, returnpacket.getLength());
+        		  
+        		  while (!s.equals("done")) {
+        			  out.println(s);
+        			  datasocket.receive(returnpacket);
+        			  s = new String(returnpacket.getData());
+        			  s = s.substring(0, returnpacket.getLength());
+        		  }
+        		  
+        		  
+        		  datasocket.close();
+        		  
+        	  }
+        	  
+            // appropriate responses form the server
+          } else if (tokens[0].equals("list")) {
+            // TODO: send appropriate command to the server and display the
+            // appropriate responses form the server
+        	
+        	  if(mode.equals("T")) {
+        		  server = new Socket(hostAddress, tcpPort);
+        		  pout = new PrintStream (server.getOutputStream());   	
+        		  din = new Scanner(server.getInputStream());
+        		  pout.println(tokens[0] + " " + tokens[1]);
+        		  
+        		  String s = din.nextLine();
+        		  
+        		  if(!s.contains("\"") && !s.equals("done")) {
+        			  out.println(s);
+        		  }
+        		  
+        		  else {
+            		  while(!s.equals("done")) {
+            			  out.println(s);
+            			  s = din.nextLine();
+            		  }
+        		  }
+
+        		  server.close();
+        	  }
+        	  else {
+        		  DatagramSocket datasocket = new DatagramSocket();
+        		  buf = new byte[(tokens[0] + " " + tokens[1]).length()];
+        		  buf = (tokens[0] + " " + tokens[1]).getBytes();
+        		  DatagramPacket datapacket,returnpacket;
+        		  
+        		  datapacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(hostAddress), udpPort);
+        		  
+        		  datasocket.send(datapacket);
+        		  
+        		  
+        		  returnpacket = new DatagramPacket(rbuf,rbuf.length);
+        		  datasocket.receive(returnpacket);
+        		  
+        		  String s = new String(returnpacket.getData());
+        		  s = s.substring(0,returnpacket.getLength());
+        		  
+        		  if(!s.contains("\"") && !s.equals("done")) {
+        			  out.println(s);
+        		  }
+        		  else {
+        			  while(!s.equals("done")) {
+        				  out.println(s);
+        				  returnpacket = new DatagramPacket(rbuf,rbuf.length);
+        				  datasocket.receive(returnpacket);
+        				  s = new String(returnpacket.getData());
+        				  s = s.substring(0,returnpacket.getLength());
+        				  
+        			  }
+        		  }
+        		  
+        		  
+        		  datasocket.close();
+        		  
+        	  }
+        	  
+        	  
+          } else if (tokens[0].equals("exit")) {
+            // TODO: send appropriate command to the server 
+        	  if(mode.equals("T")) {
+        		  server = new Socket(hostAddress, tcpPort);
+        		  pout = new PrintStream (server.getOutputStream());   	
+        		  pout.println(tokens[0]);
+        		  
+        		  server.close();
+        		  out.close();
+        		  return;
+        	  
+        	  }
+        	  else {      		  
+        		  DatagramSocket datasocket = new DatagramSocket();
+        		  buf = new byte[(tokens[0]).length()];
+        		  buf = (tokens[0]).getBytes();
+        		  DatagramPacket datapacket;
+        		  
+        		  datapacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(hostAddress), udpPort);
+        		  
+        		  datasocket.send(datapacket);                		  
+        		  
+        		  out.close();
+        		  datasocket.close();
+        		  return;
+        	  }
+        	  
+          } else {
+            System.out.println("ERROR: No such command");
+          }
+        }
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+  }
+}
---
+++ BookServer.java
@@ -0,0 +1,373 @@
+/*
+ * //UT-EID1= av28837
+ * //UT-EID2= rg36763
+ * 
+ */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BookServer extends Thread{
+	static Map<String, Integer> inventory = Collections.synchronizedMap(new HashMap<String, Integer>());
+	static Map<String, ArrayList<Integer>> userRecord = Collections.synchronizedMap(new HashMap<String, ArrayList<Integer>>());
+	static Map<Integer, String> IdToBook = Collections.synchronizedMap(new HashMap<Integer, String>());
+	static Map<Integer, String> IdToUser = Collections.synchronizedMap(new HashMap<Integer, String>());
+	static ArrayList<String> bookOrder = new ArrayList<>();
+	static BookServer lock = new BookServer();
+
+	public Socket s = new Socket();
+	static AtomicInteger record_id = new AtomicInteger(1);
+	
+	public static DatagramSocket datasocket;
+	
+	
+	public static void main(String[] args) {
+		
+		int tcpPort;
+		int udpPort;
+		if (args.length != 1) {
+			System.out.println("ERROR: Provide 1 argument: input file containing initial inventory");
+			System.exit(-1);
+		}
+		String fileName = args[0];
+		tcpPort = 7000;
+		udpPort = 8000;
+
+		// parse the inventory file
+		BufferedReader br;
+		try {
+			datasocket = new DatagramSocket(udpPort);
+			br = new BufferedReader(new FileReader(fileName));
+
+			String currentLine;
+
+			while ((currentLine = br.readLine()) != null) {
+				inventory.put(currentLine.substring(0, currentLine.lastIndexOf("\"") + 1), Integer.parseInt(currentLine.substring(currentLine.lastIndexOf("\"") + 1).trim()));
+				bookOrder.add(currentLine.substring(0, currentLine.lastIndexOf("\"") + 1));
+			}
+		} catch (IOException e) {
+		}
+		
+		BookServer bs = new BookServer();
+		BookServer.UDPServer udps = bs.new UDPServer();
+		udps.spin = true;
+		Thread t_udp = udps;
+		t_udp.start();
+
+		
+		Socket localS;
+		
+		
+		try {
+			ServerSocket listener = new ServerSocket (tcpPort);
+			while((localS = listener.accept()) != null) {
+	
+				//System.out.println("connected");
+				
+				BookServer temp = new BookServer();
+				temp.s = localS;
+				
+				Thread t = temp;
+				t.start();
+				
+			}
+	
+		} catch (IOException e) {
+			
+		}
+		
+
+		// TODO: handle request from clients
+	}
+	
+	// TCP
+	public void run() {
+		synchronized(lock) {
+			
+			try {
+				Scanner sc = new Scanner (s.getInputStream());
+				PrintStream pout = new PrintStream(s.getOutputStream());
+				String command = sc.nextLine();
+				
+				String [] tokens = command.split(" ");
+				
+				if(tokens[0].equals("borrow")) {
+					String book = tokens[2];
+					
+					for(int i=3; i < tokens.length; i++) {
+						book += " " + tokens[i];
+					}
+					
+					if(inventory.get(book) == null) {	
+						pout.println("Request Failed - We do not have this book");
+					}
+					else if(inventory.get(book) == 0) {
+						pout.println("Request Failed - Book not available");
+					}
+					
+					else {
+						pout.println("Your request has been approved, " + record_id.get() + " " + tokens[1] + " " + book);
+						
+						inventory.replace(book, inventory.get(book) - 1);
+						IdToBook.put(record_id.get(), book);						// Map ID to book
+						
+						if(userRecord.containsKey(tokens[1])) {						// Place ID in user's checked-out list
+							userRecord.get(tokens[1]).add(record_id.get());
+						}
+						else {
+							userRecord.put(tokens[1], new ArrayList<Integer>());
+							userRecord.get(tokens[1]).add(record_id.get());
+						}
+						
+						IdToUser.put(record_id.get(), tokens[1]);
+						record_id.set(record_id.get() + 1);	
+					}				
+				}
+				
+				
+				
+				else if(tokens[0].equals("return")) {
+					
+					if(!IdToBook.containsKey(Integer.parseInt(tokens[1]))) {
+						pout.println(tokens[1] + " " + "not found, no such borrow record");
+					}
+					else {
+						inventory.replace(IdToBook.get(Integer.parseInt(tokens[1])), inventory.get(IdToBook.get(Integer.parseInt(tokens[1]))) + 1);
+						userRecord.get(IdToUser.get(Integer.parseInt(tokens[1]))).remove((Object)Integer.parseInt(tokens[1]));
+						pout.println(tokens[1] + " " + "is returned");
+					}
+			
+				}
+				
+				else if(tokens[0].equals("list")) {
+					
+					if(!userRecord.containsKey(tokens[1])) {
+						pout.println("No record found for " + tokens[1]);
+					}
+					else {
+						for(int i = 0; i < userRecord.get(tokens[1]).size(); i++) {
+							pout.println( userRecord.get(tokens[1]).get(i) + " " + IdToBook.get(userRecord.get(tokens[1]).get(i)));
+						}
+						pout.println("done");
+					}		
+				}
+				
+				
+				else if(tokens[0].equals("inventory")) {			
+					
+					for(int i=0; i<bookOrder.size(); i++) {
+						pout.println(bookOrder.get(i) + " " + inventory.get(bookOrder.get(i)));
+					}
+					
+					pout.println("done");
+				}
+				
+				else {
+					PrintWriter out = new PrintWriter(new FileOutputStream("inventory.txt")); 
+					
+					for(int i=0; i<bookOrder.size(); i++) {
+						out.println(bookOrder.get(i) + " " + inventory.get(bookOrder.get(i)));					
+					}
+					
+					out.close();
+				}
+				
+				
+			} catch (IOException e) {
+			}
+			
+		}
+		
+	}
+	
+	
+	class UDPServer extends Thread{
+		int len = 1024;
+		DatagramPacket datapacket, returnpacket;
+		byte [] buf = new byte [len];		
+		Boolean spin = false;
+		
+		
+		// UDP
+		public void run() {
+			if(spin) {
+				try {				
+					while(true) {			
+						datapacket = new DatagramPacket(buf, buf.length);
+						datasocket.receive(datapacket);	
+						
+						UDPServer temp = new UDPServer();
+						temp.datapacket = datapacket;
+						temp.start();
+					}
+			
+					
+				} catch (Exception e) {
+				}
+							
+			}
+			
+			
+			synchronized (lock) {
+				
+				try {
+					
+					
+					String command = new String(datapacket.getData());
+					command = command.substring(0, datapacket.getLength());
+					
+					String [] tokens = command.split(" ");
+					
+					if(tokens[0].equals("borrow")) {
+						String book = tokens[2];
+						
+						for(int i=3; i < tokens.length; i++) {
+							book += " " + tokens[i];
+						}
+						
+						if(inventory.get(book) == null) {	
+							//pout.println("Request Failed - We do not have this book");
+							buf = new byte ["Request Failed - We do not have this book".length()];
+							buf = "Request Failed - We do not have this book".getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}
+						else if(inventory.get(book) == 0) {
+							//pout.println("Request Failed - Book not available");
+							buf = new byte ["Request Failed - Book not available".length()];
+							buf = "Request Failed - Book not available".getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}
+						
+						else {
+							//pout.println("Your request has been approved, " + record_id.get() + " " + tokens[1] + " " + book);
+							
+							buf = new byte [("Your request has been approved, " + record_id.get() + " " + tokens[1] + " " + book).length()];
+							buf = ("Your request has been approved, " + record_id.get() + " " + tokens[1] + " " + book).getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							
+							
+							inventory.replace(book, inventory.get(book) - 1);
+							IdToBook.put(record_id.get(), book);						// Map ID to book
+							
+							if(userRecord.containsKey(tokens[1])) {						// Place ID in user's checked-out list
+								userRecord.get(tokens[1]).add(record_id.get());
+							}
+							else {
+								userRecord.put(tokens[1], new ArrayList<Integer>());
+								userRecord.get(tokens[1]).add(record_id.get());
+							}
+							
+							datasocket.send(returnpacket);
+							IdToUser.put(record_id.get(), tokens[1]);
+							record_id.set(record_id.get() + 1);	
+						}				
+					}
+					
+					
+					
+					else if(tokens[0].equals("return")) {
+						
+						if(!IdToBook.containsKey(Integer.parseInt(tokens[1]))) {
+							//pout.println(tokens[1] + " " + "not found, no such borrow record");
+							buf = new byte [(tokens[1] + " " + "not found, no such borrow record").length()];
+							buf = (tokens[1] + " " + "not found, no such borrow record").getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}
+						else {
+							inventory.replace(IdToBook.get(Integer.parseInt(tokens[1])), inventory.get(IdToBook.get(Integer.parseInt(tokens[1]))) + 1);
+							userRecord.get(IdToUser.get(Integer.parseInt(tokens[1]))).remove((Object)Integer.parseInt(tokens[1]));
+							//pout.println(tokens[1] + " " + "is returned");
+							buf = new byte [(tokens[1] + " " + "is returned").length()];
+							buf = (tokens[1] + " " + "is returned").getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}
+				
+					}
+					
+					else if(tokens[0].equals("list")) {
+						
+						if(!userRecord.containsKey(tokens[1])) {
+							//pout.println("No record found for " + tokens[1]);
+							buf = new byte [("No record found for " + tokens[1]).length()];
+							buf = ("No record found for " + tokens[1]).getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}
+						else {
+							for(int i = 0; i < userRecord.get(tokens[1]).size(); i++) {
+								//pout.println( userRecord.get(tokens[1]).get(i) + " " + IdToBook.get(userRecord.get(tokens[1]).get(i)));
+								buf = new byte [( userRecord.get(tokens[1]).get(i) + " " + IdToBook.get(userRecord.get(tokens[1]).get(i))).length()];
+								buf = ( userRecord.get(tokens[1]).get(i) + " " + IdToBook.get(userRecord.get(tokens[1]).get(i))).getBytes();
+								returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+								datasocket.send(returnpacket);
+							}
+							//pout.println("done");
+							buf = new byte [("done").length()];
+							buf = ("done").getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}		
+					}
+					
+					
+					else if(tokens[0].equals("inventory")) {			
+						
+						for(int i=0; i<bookOrder.size(); i++) {
+							//pout.println(bookOrder.get(i) + " " + inventory.get(bookOrder.get(i)));
+							buf = new byte [(bookOrder.get(i) + " " + inventory.get(bookOrder.get(i))).length()];
+							buf = (bookOrder.get(i) + " " + inventory.get(bookOrder.get(i))).getBytes();
+							returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+							datasocket.send(returnpacket);
+						}
+						
+						//pout.println("done");
+						buf = new byte [("done").length()];
+						buf = ("done").getBytes();
+						returnpacket = new DatagramPacket(buf, buf.length, datapacket.getAddress(), datapacket.getPort());
+						datasocket.send(returnpacket);
+					}
+					
+					else {
+						PrintWriter out = new PrintWriter(new FileOutputStream("inventory.txt")); 
+						
+						for(int i=0; i<bookOrder.size(); i++) {
+							out.println(bookOrder.get(i) + " " + inventory.get(bookOrder.get(i)));					
+						}
+						
+						out.close();
+					}
+					
+
+					
+					
+				} catch (IOException e) {
+				}
+				
+			}
+
+		}
+		
+	}	
+	
+}
